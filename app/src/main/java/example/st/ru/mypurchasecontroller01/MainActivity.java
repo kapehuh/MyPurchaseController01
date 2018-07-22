77e2c536b2c226012f35ce594872b553df3d9196
St <kapehuh@gmail.com>
2018-07-23 00:42:17 +0300

    Somthing wrong...

--- app/src/main/java/example/st/ru/mypurchasecontroller01/MainActivity.java
+++ app/src/main/java/example/st/ru/mypurchasecontroller01/MainActivity.java
@@ -1,17 +1,28 @@
 package example.st.ru.mypurchasecontroller01;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.vision.barcode.Barcode;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends Activity {
     TextView barcodeResult;
+    TextView productInfo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +30,7 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
         barcodeResult=(TextView)findViewById(R.id.barcode_result);
+        productInfo=(TextView)findViewById(R.id.product_info);
     }
 
     //add event barcode button
@@ -28,8 +40,10 @@ public class MainActivity extends Activity {
         startActivityForResult(intent,0);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         if (requestCode==0){
             if (resultCode== CommonStatusCodes.SUCCESS){
                 if (data!=null){
@@ -40,6 +54,14 @@ public class MainActivity extends Activity {
                 barcodeResult.setText("No barcode found");
             }
         }
+
+        //getting data from firestore
+
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        CollectionReference frDb=db.collection("goods");
+        Query goodesBarcode = frDb.whereEqualTo("barcode",barcodeResult);
+
         super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
